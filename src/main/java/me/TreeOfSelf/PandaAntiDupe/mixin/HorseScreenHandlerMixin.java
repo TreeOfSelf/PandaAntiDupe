21f17b5463a39e5455fcb5b5abbff526d0a2eeb1
Sebastian <sebastian97@gmail.com>
2025-12-11 17:29:52 -0800

    Mixin fixes

--- src/main/java/me/TreeOfSelf/PandaAntiDupe/mixin/HorseScreenHandlerMixin.java
+++ src/main/java/me/TreeOfSelf/PandaAntiDupe/mixin/HorseScreenHandlerMixin.java
@@ -1,10 +1,12 @@
 package me.TreeOfSelf.PandaAntiDupe.mixin;
 
 import me.TreeOfSelf.PandaAntiDupe.PandaAntiDupeConfig;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.HorseScreenHandler;
+import net.minecraft.screen.MountScreenHandler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,12 +14,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(HorseScreenHandler.class)
+@Mixin(MountScreenHandler.class)
 public class HorseScreenHandlerMixin {
-
-    @Shadow
-    @Final
-    private AbstractHorseEntity entity;
+    @Shadow @Final protected LivingEntity mount;
 
     @Inject(
             method = "quickMove",
@@ -27,7 +26,7 @@ public class HorseScreenHandlerMixin {
     private void onlyTransferIfEntityAlive(PlayerEntity p, int i, CallbackInfoReturnable<ItemStack> cir) {
         if (!PandaAntiDupeConfig.getDupeStatus("HorseQuickMoveDupe")) return;
 
-        if ((p.isDead() || p.isRemoved() || entity.isDead() || entity.isRemoved()))
+        if ((p.isDead() || p.isRemoved() || mount.isDead() || mount.isRemoved()))
             cir.setReturnValue(ItemStack.EMPTY);
     }
 }
--- src/main/java/me/TreeOfSelf/PandaAntiDupe/mixin/ShulkerBoxBlockMixin.java
+++ src/main/java/me/TreeOfSelf/PandaAntiDupe/mixin/ShulkerBoxBlockMixin.java
@@ -1,26 +1,35 @@
 package me.TreeOfSelf.PandaAntiDupe.mixin;
 
 import me.TreeOfSelf.PandaAntiDupe.PandaAntiDupeConfig;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.context.LootContextParameters;
+import net.minecraft.loot.context.LootWorldContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 @Mixin(ShulkerBoxBlock.class)
 public class ShulkerBoxBlockMixin {
     @Inject(
-            method = "method_10524",
+            method = "getDroppedStacks",
             at = @At("TAIL"),
             remap = false
     )
-    private static void clearAfterGetDroppedStack(ShulkerBoxBlockEntity shulkerBoxBlockEntity,
-                                                     Consumer lootConsumer, CallbackInfo ci) {
+    private void clearAfterGetDroppedStack(BlockState state, LootWorldContext.Builder builder, CallbackInfoReturnable<List<ItemStack>> cir) {
         if (!PandaAntiDupeConfig.getDupeStatus("ShulkerBoxDupe")) return;
 
-        shulkerBoxBlockEntity.clear();
+        BlockEntity blockEntity = builder.getOptional(LootContextParameters.BLOCK_ENTITY);
+        if (blockEntity instanceof ShulkerBoxBlockEntity shulkerBoxBlockEntity) {
+            shulkerBoxBlockEntity.clear();
+        }
     }
 }
